46b2391e253755e30799536e02e6f13027a4a12d
brollins90 <brollins90@gmail.com>
2015-10-27 09:53:12 -0600

    linear forecaster works by adding 1

--- src/main/java/copsandrobbers/forecast/LinearForecaster.java
+++ src/main/java/copsandrobbers/forecast/LinearForecaster.java
@@ -8,10 +8,10 @@ public class LinearForecaster implements Forecaster {
     private int lastSeen;
 
     public Coordinate getNextPrediction() {
-        return new Coordinate(lastSeen + 1, lastSeen + 1);
+        return new Coordinate(lastSeen + 1, lastSeen + 2);
     }
 
     public void seePrevious(Coordinate coordinate) {
-        lastSeen = coordinate.getX();
+        lastSeen = coordinate.getY();
     }
 }
